7eac61dba6be92dc93e396bac64f705c2fa46302
arjuna1011 <jgboatright@att.net>
2023-06-08 14:13:26 -0400

    Final touches

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-const element = document.querySelector('.element');
-element.classList.add('swirl');
+document.addEventListener('DOMContentLoaded', () => {
+    const videoElement = document.getElementById('video');
+    const button = document.getElementById('button');
+  
+    async function selectMediaStream() {
+      try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+          videoElement.play();
+        };
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  
+    button.addEventListener('click', async () => {
+      button.disabled = true;
+      await videoElement.requestPictureInPicture();
+      button.disabled = false;
+    });
+  
+    selectMediaStream();
+  });
+  
